255b4ace4256b73d7c603141ad3b191d64930005
sayyedFaiz <sayyedfaiz7@gmail.com>
2023-10-25 20:41:43 +0200

    landing zone

---
+++ Operation/landing_zone.py
@@ -0,0 +1,43 @@
+import datetime
+
+
+def ingestinTime():
+    now = datetime.datetime.now()
+    now = now.strftime("%Y_%m_%d_%H_%M_%S")
+    return now
+
+
+barca_rent = pd.read_csv(
+    "C:/Users/sayye/OneDrive/Desktop/college/PG/UPC/ADSDB/temporal/Barcelona_rent_price.csv"
+)
+years = {2018, 2019, 2020}
+
+for year in years:
+  rent_df = barca_rent.loc[barca_rent['Year'] == year]
+  rent_df.reset_index(drop=True, inplace=True)
+  rent_df.to_csv(f'C:/Users/sayye/OneDrive/Desktop/college/PG/UPC/ADSDB/Persistent/rents_{year}_{ingestinTime()}.csv', index=False)
+
+
+
+income_2018 = pd.read_csv(
+    "C:/Users/sayye/OneDrive/Desktop/college/PG/UPC/ADSDB/temporal/2018_atles_renda_bruta_persona.csv"
+)
+income_2019 = pd.read_csv(
+    "C:/Users/sayye/OneDrive/Desktop/college/PG/UPC/ADSDB/temporal/2019_atles_renda_bruta_persona.csv"
+)
+income_2020 = pd.read_csv(
+    "C:/Users/sayye/OneDrive/Desktop/college/PG/UPC/ADSDB/temporal/2020_atles_renda_bruta_persona.csv"
+)
+
+income_2018.to_csv(
+    f"C:/Users/sayye/OneDrive/Desktop/college/PG/UPC/ADSDB/Persistent/income_2018_{ingestinTime()}.csv",
+    index=False,
+)
+income_2019.to_csv(
+    f"C:/Users/sayye/OneDrive/Desktop/college/PG/UPC/ADSDB/Persistent/income_2019_{ingestinTime()}.csv",
+    index=False,
+)
+income_2020.to_csv(
+    f"C:/Users/sayye/OneDrive/Desktop/college/PG/UPC/ADSDB/Persistent/income_2020_{ingestinTime()}.csv",
+    index=False,
+)
