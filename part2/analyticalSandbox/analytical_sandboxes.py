fa89e240c8e9d9ce0d1d15254d3a3ab569463df3
sayyedFaiz <sayyedfaiz7@gmail.com>
2023-12-19 21:08:31 +0100

    analytical sandox DB

--- part2/analyticalSandbox/analytical_sandboxes.py
+++ part2/analyticalSandbox/analytical_sandboxes.py
@@ -35,11 +35,47 @@ def prepare_final_dataset(income_df, rent_df):
 
 
 
-def create_new_table_from_df(con, df, table_name):
+# def create_new_table_from_df(con, df, table_name):
+#     con.register('temp_table', df)
+#     query = f"CREATE TABLE {table_name} AS SELECT * FROM temp_table;"
+#     con.execute(query)
+#     con.unregister('temp_table')
+
+def create_new_table_from_df(con, df, table_name, unique_columns):
+    # Register the DataFrame as a virtual table in DuckDB
     con.register('temp_table', df)
-    query = f"CREATE TABLE {table_name} AS SELECT * FROM temp_table;"
-    con.execute(query)
+
+    # Check if the table already exists
+    if not con.execute(f"SELECT * FROM information_schema.tables WHERE table_name = '{table_name}'").fetchone():
+        # If the table doesn't exist, create a new table from the virtual table
+        con.execute(f"CREATE TABLE {table_name} AS SELECT * FROM temp_table")
+        print(f"Table {table_name} created.")
+    else:
+        # If the table exists, append the data into the existing table
+        con.execute(f"INSERT INTO {table_name} SELECT * FROM temp_table")
+        print(f"Data appended to table {table_name}.")
+        # Now remove duplicates
+        # Assuming 'unique_columns' is a list of column names that uniquely identify a row
+        unique_cols = ', '.join(unique_columns)
+        deduplicate_query = f"""
+        CREATE TEMPORARY TABLE deduped_temp AS
+        SELECT DISTINCT ON ({unique_cols}) *
+        FROM {table_name}
+        ORDER BY {unique_cols};
+
+        DELETE FROM {table_name};
+
+        INSERT INTO {table_name}
+        SELECT * FROM deduped_temp;
+
+        DROP TABLE deduped_temp;
+        """
+        con.execute(deduplicate_query)
+        print(f"Removed duplicates from table {table_name}.")
+
+    # Unregister the virtual table to clean up namespace
     con.unregister('temp_table')
+
 
 # append to the tables if it exist or else create
 
@@ -74,10 +110,10 @@ def main():
     df_analysis = prepare_final_dataset(income_sandbox, rents_sandbox)
     close_database_connection(con)
 
-
+    unique_columns = ['district', 'neighbourhood', 'year']
     con = connect_to_database('analyticalSandbox/analytical_sandboxes.db')
-    create_new_table_from_df(con, df_analysis, 'sandbox')
-    schema_df, profile_df = retrieve_schema_and_profile(connect_to_database('analyticalSandbox/analytical_sandboxes.db'), 'sandbox')
+    create_new_table_from_df(con, df_analysis, 'sandbox', unique_columns)
+    schema_df, profile_df = retrieve_schema_and_profile(con, 'sandbox')
     close_database_connection(con)
 
     # print("Schema:", schema_df)
