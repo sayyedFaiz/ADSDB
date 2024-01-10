4c0ab4eefadb73cf99ccf48bebc9dabef6525e6a
sayyedFaiz <sayyedfaiz7@gmail.com>
2024-01-10 21:35:51 +0100

    successfull run uploadting without csv and pickle file

--- part2/master.py
+++ part2/master.py
@@ -14,15 +14,11 @@ def run_script(script_path):
 def main():
     scripts_to_run = [
         'analyticalSandbox/analytical_sandboxes.py',
-        # 'analyticalSandbox/analytical_sandbox_profiling.py',
         'featureGeneration/featureGeneration/featuregeneration.py',
         'featureGeneration/dataPreparation/datapreparation.py',
         'featureGeneration/datalabelling/labeling.py',
-        # 'featureGeneration/trainingAndValidationDatasets/training_testing_sets.py',
-        # 'featureGeneration/trainingAndValidationDatasets/training_valitdation_profiling.py',
         'trainingAndValidation/trainingAndTesting/machine_learning.py',
-        # 'trainingAndValidation/visualization/visualize_model_comparison.py'
-        'advanceTopic/featureSelection/pca.py',
+        # 'advanceTopic/featureSelection/pca.py',
         # 'advanceTopic/featureSelection/machine_learning.py',
     ]
 
